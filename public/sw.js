ea2cacb2f37672611b5ec44e99cef00de8d3e3dc
Сергей Петров <sergejpetrov@Sergejs-MacBook-Air-431.local>
2026-08-29 16:20:58 +0300

    Prevent iPhone input zoom

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'fence-estimate-v6';
+const CACHE_NAME = 'fence-estimate-v7';
 const APP_SHELL = ['/', '/manifest.webmanifest', '/favicon.svg'];
 
 self.addEventListener('install', (event) => {
