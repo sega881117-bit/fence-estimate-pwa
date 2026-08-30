c35a15e8a69acd6fc58bc38824183bb342916a1b
Сергей Петров <sergejpetrov@Sergejs-MacBook-Air-431.local>
2026-08-30 15:43:51 +0300

    Vertically center estimate amounts

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'fence-estimate-v15';
+const CACHE_NAME = 'fence-estimate-v16';
 const APP_SHELL = ['./', './manifest.webmanifest', './favicon.svg'];
 
 self.addEventListener('install', (event) => {
