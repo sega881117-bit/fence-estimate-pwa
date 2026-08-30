4b7100c6bab664fc146aebfa67f266cd2bd1a951
Сергей Петров <sergejpetrov@Sergejs-MacBook-Air-431.local>
2026-08-30 15:01:55 +0300

    Restore table lines and center values

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'fence-estimate-v13';
+const CACHE_NAME = 'fence-estimate-v14';
 const APP_SHELL = ['./', './manifest.webmanifest', './favicon.svg'];
 
 self.addEventListener('install', (event) => {
