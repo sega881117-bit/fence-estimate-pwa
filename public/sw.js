596bbddaf10ea58361d17b90d4ff3b8b2d17c821
Сергей Петров <sergejpetrov@Sergejs-MacBook-Air-431.local>
2026-09-01 14:39:20 +0300

    Widen estimate result layout

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'fence-estimate-v19';
+const CACHE_NAME = 'fence-estimate-v20';
 const APP_SHELL = ['./', './manifest.webmanifest', './favicon.svg'];
 
 self.addEventListener('install', (event) => {
